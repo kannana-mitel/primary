7bc9815ea6d9a349853dd76a10afa565ab109a77
kannana-mitel <kannana@akdriods-mac-2.lan>
2020-04-06 14:15:10 -0400

    Initial Angular App

---
+++ myNewApp/src/app/modules/EagerModule1/EagerModule1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-EagerModule1',
+  templateUrl: './EagerModule1.component.html',
+  styleUrls: ['./EagerModule1.component.css']
+})
+export class EagerModule1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ myNewApp/src/app/modules/EagerModule1/lazy1/lazy1.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Lazy1Component } from './lazy1.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Lazy1Component]
+})
+export class Lazy1Module { }
